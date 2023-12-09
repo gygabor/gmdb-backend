0042c6621db56ff116ab2e9f06d1ca3465e41863
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-09 19:50:13 +0100

    feat(moviesController): fetch movies

--- src/api/movies/moviesController.ts
+++ src/api/movies/moviesController.ts
@@ -1,3 +1,16 @@
-const moviesController = () => {}
+import { fetchMovies } from './services'
+import { Movie } from './types'
+
+interface Props {
+  query?: string
+  page?: string
+}
+
+const moviesController = async ({
+  query = '',
+  page = '1',
+}: Props): Promise<Movie[] | string> => {
+  return await fetchMovies(query, page)
+}
 
 export default moviesController
