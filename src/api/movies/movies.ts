a198e125e029d3aca3d9e9e3f7f686f40ba1bd93
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-09 19:47:39 +0100

    feat(movies): add route, handle request

--- src/api/movies/movies.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'express'
-import moviesController from './moviesController'
-
-const movies = async (req: Request, res: Response) => {
-  await moviesController()
-  res.send('Movies!')
-}
-
-export default movies
