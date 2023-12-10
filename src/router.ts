139797f64a0865fc393bc5353b5711e029a74ba2
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-10 23:32:41 +0100

    feat: use movies router

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,6 @@ import { helloWorld, movies } from './api'
 const router = express.Router()
 
 router.get('/', helloWorld)
-router.get('/movies', movies)
+router.use('/movies', movies)
 
 export default router
