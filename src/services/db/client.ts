7208be0eb836dfb4de4e6a4108e59780a343672f
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-11 15:50:16 +0100

    fix(db client): connect to db

--- src/services/db/client.ts
+++ src/services/db/client.ts
@@ -5,6 +5,7 @@ const config = {
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+  database: 'gmdb',
 }
 
 const client = new Client(config)
