176611479d8665beea00f7a604f59806183b7527
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-11 15:56:33 +0100

    feat(queries): update queries

--- src/api/movies/search/services/queries.ts
+++ src/api/movies/search/services/queries.ts
@@ -1,5 +1,15 @@
-const queries = {
-  save: 'INSERT INTO movie_searches (query, page, results, time) VALUES ($1, $2, $3, $4)',
+interface Queries {
+  insertMovies: string
+  getMovies: string
+  updateCount: string
+}
+
+const queries: Queries = {
+  insertMovies:
+    'INSERT INTO movie_searches (query, page, response, time) VALUES ($1, $2, $3, $4)',
+  getMovies: `SELECT id, response FROM movie_searches WHERE query = $1 AND page = $2 AND time > NOW() - INTERVAL '2 minutes'`,
+  updateCount:
+    'UPDATE movie_searches SET request_count = request_count + 1 WHERE id = $1',
 }
 
 export default queries
