b1c0bee22a1c9a4524b2b7679cbe107a10875f6a
gygabor <gyorgyg@gmail.com>
2023-12-13 23:20:30 +0100

    feat: install cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import router from '@src/router'
 import db from '@src/services/db'
 
 const app = express()
 
+app.use(cors())
 app.use(router)
 
 db.connectToDb()
