3b43ac41f4fc52545d0060b9d409e79d8415ca63
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-18 21:46:06 +0100

    feat(getMovies): add tests

---
+++ src/api/movies/search/services/tests/getMovies.test.ts
@@ -0,0 +1,41 @@
+import { responseDb } from '../../tests/mockedValues'
+import getMovies from '../getMovies'
+import queries from '../queries'
+import queryDb from '../queryDb'
+
+const mockedQueryDb = queryDb as jest.MockedFunction<typeof queryDb>
+
+jest.mock('../queryDb', () => {
+  return jest.fn(() => Promise.resolve({ rows: [] }))
+})
+
+const sqlQuery = queries['getMovies']
+
+describe('getMovies', () => {
+  it('calls queryDb', async () => {
+    await getMovies('test', '1')
+
+    expect(mockedQueryDb).toHaveBeenCalledWith(sqlQuery, ['test', '1'])
+  })
+
+  it('returns null', async () => {
+    await expect(getMovies('test', '1')).resolves.toEqual(null)
+  })
+
+  it('returns a result', async () => {
+    const id = '1'
+
+    mockedQueryDb.mockResolvedValue({
+      rows: [{ id, response: responseDb }],
+      rowCount: 0,
+      command: '',
+      oid: 0,
+      fields: [],
+    })
+
+    await expect(getMovies('test', '1')).resolves.toEqual({
+      id,
+      response: responseDb,
+    })
+  })
+})
