b637479aaddff0e74381ead5400be749d3163073
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-18 22:02:04 +0100

    fix: link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,2 +1,2 @@
 export const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
-export const TMDB_MOVIE_SEARCH_URL = '/movies/search'
+export const TMDB_MOVIE_SEARCH_URL = '/search/movie'
--- src/services/axios/__mocks__/index.ts
+++ src/services/axios/__mocks__/index.ts
@@ -1,6 +1,8 @@
+import { TMDB_MOVIE_SEARCH_URL } from '@src/constants/links'
+
 const client = {
   get: jest.fn((url) => {
-    if (url.includes('movies/search')) {
+    if (url.includes(TMDB_MOVIE_SEARCH_URL)) {
       return Promise.resolve({
         data: { page: 1, results: [], total_pages: 1, total_results: 0 },
       })
